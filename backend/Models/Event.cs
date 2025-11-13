88b7104d3adccba9f1a9255c4f107e9c4126a4b6
Siudyy <97389155+Siudyy@users.noreply.github.com>
2025-11-13 13:24:27 +0100

    update
    added location to events

--- backend/Models/Event.cs
+++ backend/Models/Event.cs
@@ -8,12 +8,14 @@ public class Event
     [Key]
     public int EventId { get; set; }
     public string Title { get; set; } = default!;
+    public string  Location { get; set; } = default!;
     public string Description { get; set; } = default!;
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
+    
     public int CreatedBy { get; set; }
 
     [ForeignKey("CreatedBy")]
-    public Employee Creator { get; set; } = default!;
+    public Employee? Creator { get; set; }
     public ICollection<EventParticipation> Participants { get; set; } = new List<EventParticipation>();
 }
