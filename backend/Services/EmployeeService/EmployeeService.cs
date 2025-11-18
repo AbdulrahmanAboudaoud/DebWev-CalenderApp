731464c21e479af4b1ac17fbd7da9425638523a9
Telmo Claro <telmoclaro@proton.me>
2025-11-18 16:26:17 +0100

    CRUD works
    I think

--- backend/Services/EmployeeService/EmployeeService.cs
+++ backend/Services/EmployeeService/EmployeeService.cs
@@ -1,38 +1,60 @@
 using backend.Models;
 using backend.Repository;
+using DebWev_CalenderApp.DTOs;
 
 namespace backend.Services.EmployeeService
 {
     public class EmployeeService : IEmployeeService
     {
         private readonly IRepository<Employee> _employeeRepo;
-
         public EmployeeService(IRepository<Employee> employeeRepo)
         {
             _employeeRepo = employeeRepo;
         }
-        
-        public IEnumerable<Employee> GetAllEmployees()
-        {
-            throw new NotImplementedException();
-        }
-        public Employee? GetEmployeeById(int id)
-        {
-            throw new NotImplementedException();
-        }
-        public Employee? AddEmployee(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
 
-        public bool DeleteEmployee(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<Employee> GetAll() => _employeeRepo.GetAll();
 
-        public Employee? UpdateEmployee(Employee employee)
+        public Employee? GetById(int id) => _employeeRepo.GetById(id);
+
+        public Employee? Add(Employee employee)
         {
-            throw new NotImplementedException();
+            _employeeRepo.Add(employee);
+            _employeeRepo.SaveChanges();
+            return employee;
+        }
+        public Employee? Update(int id, EmployeeDto employee)
+        {
+            var existingEmployee = _employeeRepo.GetById(id);
+            if (existingEmployee == null) return null;
+
+            existingEmployee.Name = employee.name;
+            existingEmployee.Email = employee.email;
+            existingEmployee.Role = employee.role;
+            existingEmployee.Department = employee.department;
+            existingEmployee.Password = employee.password;
+            
+            _employeeRepo.Update(existingEmployee);
+            _employeeRepo.SaveChanges();
+            return existingEmployee;
+        }
+        public bool DeleteById(int id)
+        {
+            var employee = _employeeRepo.GetById(id);
+            if (employee == null) return false;
+
+            _employeeRepo.Delete(employee);
+            _employeeRepo.SaveChanges();
+            return true;
+        }
+        public bool DeleteAll()
+        {
+            var employees = _employeeRepo.GetAll();
+            foreach (var employee in employees)
+            {
+                _employeeRepo.Delete(employee);
+            }
+            _employeeRepo.SaveChanges();
+            return true;
         }
     }
 }
--- backend/Services/EmployeeService/IEmployeeService.cs
+++ backend/Services/EmployeeService/IEmployeeService.cs
@@ -1,13 +1,15 @@
 using backend.Models;
+using DebWev_CalenderApp.DTOs;
 
 namespace backend.Services.EmployeeService
 {
     public interface IEmployeeService
     {
-        IEnumerable<Employee> GetAllEmployees();
-        Employee? GetEmployeeById(int id);
-        Employee? AddEmployee(Employee employee);
-        Employee? UpdateEmployee(Employee employee);
-        bool DeleteEmployee(int id);
+        IEnumerable<Employee> GetAll();
+        Employee? GetById(int id);
+        Employee? Add(Employee employee);
+        Employee? Update(int id, EmployeeDto employee);
+        bool DeleteById(int id);
+        bool DeleteAll();
     }
 }
